4adb2d85c2690a3fb47ab936bea47e829cc4720b
Alam García Roldán <garcia76@avaya.com>
2018-09-27 14:48:23 -0500

    GeoLocation Beta

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,29 @@
-$( document ).ready(function() {
-  $( "#callbtn" ).click(function() {
-    alert( "Handler for .click() called." );
-  });
+$(document).ready(function() {
+  if (navigator.geolocation) {
+    console.log('Soportado');
+  } else {
+    console.log('Geolocation no disponible');
+  }
 
+
+  $("#callbtn").click(function() {
+  if (!navigator.geolocation){
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    console.log(latitude);
+    console.log(longitude);
+  };
+
+  function error() {
+
+  };
+
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  });
 });
